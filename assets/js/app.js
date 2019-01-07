9ca64b29cf2688d1d13a6c66fa0a4cd0a5535ead
Glazelf <gideon.steenbergen@gmail.com>
2019-01-07 12:28:01 +0100

    helvetica standard

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,14 +17,14 @@ var x = setInterval(function() {
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Display the result in the element with id="demo"
-  document.getElementById("demo").innerHTML = `<span style='float:left;position:relative;margin-left:28%;margin-top:-8%;color:white;font-size:96px'>${days}d ${hours}h ${minutes}m ${seconds}s</span>`;
+  document.getElementById("demo").innerHTML = `<span style='font-family:helvetica;float:left;position:relative;margin-left:28%;margin-top:-8%;color:white;font-size:96px'>${days}d ${hours}h ${minutes}m ${seconds}s</span>`;
   
 
   // If the count down is finished, write some text 
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("demo").innerHTML = `<span style='float:left;position:relative;margin-left:36%;margin-top:-8%;color:white;font-size:96px'>TDT Time!</span>`;
+    document.getElementById("demo").innerHTML = `<span style='font-family:helvetica;float:left;position:relative;margin-left:36%;margin-top:-8%;color:white;font-size:96px'>TDT Time!</span>`;
   }
 }, 1000);
-document.getElementById("credits").innerHTML = `<span style='float:left;position:relative;margin-top:35%;margin-left:89.5%;color:white;font-size:16px'>Made by <a href="https://twitter.com/Glazelfy" target="_blank">Glaze</a> of the</span>`;
-document.getElementById("creditsteam").innerHTML = `<span style='float:left;position:relative;margin-top:0%;margin-left:67%;color:white;font-size:16px'>"0-2 Free Soda And Hanging With The Homies All Weekend"- Crew- Team</span>`;
+document.getElementById("credits").innerHTML = `<span style='font-family:helvetica;float:left;position:relative;margin-top:35%;margin-left:89.5%;color:white;font-size:16px'>Made by <a href="https://twitter.com/Glazelfy" target="_blank">Glaze</a> of the</span>`;
+document.getElementById("creditsteam").innerHTML = `<span style='font-family:helvetica;float:left;position:relative;margin-top:0%;margin-left:65%;color:white;font-size:16px'>"0-2 Free Soda And Hanging With The Homies All Weekend"- Crew- Team</span>`;
